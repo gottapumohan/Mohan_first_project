bdcf003d62e76a5ae056d9ccf01a8f26cec810f7
gottapumohan <gottapumohan25@gmail.com>
2026-06-20 07:41:42 +0530

    Added accountCustomerProfile LWC component

---
+++ force-app/main/default/lwc/accountCustomerProfile/accountCustomerProfile.js
@@ -0,0 +1,30 @@
+import {LightningElement, api} from 'lwc';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
+
+export default class AccountCustomerProfile
+extends LightningElement {
+
+    @api recordId;
+
+    handleSuccess(event) {
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Success',
+                message:
+                'Account updated successfully',
+                variant: 'success'
+            })
+        );
+    }
+
+    handleError(event) {
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Success',
+                message: 'Account successfully updated',
+                variant: 'success'
+            })
+        );
+    }
+
+}
